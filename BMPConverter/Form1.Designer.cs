d565e9993f1aed9e1ae74d347c450b1b689e5dd7
DLeliuhin <dleliuhin@mail.ru>
2018-04-15 14:19:43 +0300

    Add all files

---
+++ BMPConverter/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace BMPConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(178, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя файла";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BMPConverter.Properties.Resources.StartPhoto1;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 266);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(449, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(142, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Convert to Png";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(449, 72);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(135, 24);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Convert to Gif";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.Location = new System.Drawing.Point(449, 117);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(149, 24);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Convert to Jpeg";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(35, 147);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(348, 31);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(449, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 60);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 31);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Путь к файлу:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(449, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 61);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox4.Location = new System.Drawing.Point(449, 166);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(137, 24);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Convert to Tiff";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox5.Location = new System.Drawing.Point(449, 213);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(144, 24);
+            this.checkBox5.TabIndex = 13;
+            this.checkBox5.Text = "Convert to Icon";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(615, 469);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Created by DLeliuhin";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 492);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BMPConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ BMPConverter/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BMPConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(@"D:\2 курс\4 семестр\ЭВМ\CourseWorkEVM\StartPhoto1.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Загрузка формы
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //  Имя файла
+        } 
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Область для загрузки фото
+            //По умолчанию стоит фотография "Нет фото"
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                if (ofd.ShowDialog()==DialogResult.OK)
+                {
+                    richTextBox1.Text = ofd.FileName;
+                    textBox1.Text = ofd.SafeFileName;
+                    pictureBox1.Image = new Bitmap(@ofd.FileName);
+                    pictureBox1.ImageLocation = ofd.FileName;   
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Form1", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Текстовое окно, выводящее полный путь к файлу и полное его имя
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if(checkBox1.CheckState == CheckState.Checked)
+                {
+                    pictureBox1.Image.Save(Path.ChangeExtension(ofd.FileName,".Png"), System.Drawing.Imaging.ImageFormat.Png);
+                }
+                if (checkBox2.CheckState == CheckState.Checked)
+                {
+                    pictureBox1.Image.Save(Path.ChangeExtension(ofd.FileName, ".Gif"), System.Drawing.Imaging.ImageFormat.Gif);
+                }
+                if (checkBox3.CheckState == CheckState.Checked)
+                {
+                    pictureBox1.Image.Save(Path.ChangeExtension(ofd.FileName, ".Jpeg"), System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                if (checkBox4.CheckState == CheckState.Checked)
+                {
+                    pictureBox1.Image.Save(Path.ChangeExtension(ofd.FileName, ".Tiff"), System.Drawing.Imaging.ImageFormat.Tiff);
+                }
+                if (checkBox5.CheckState == CheckState.Checked)
+                {
+                    pictureBox1.Image.Save(Path.ChangeExtension(ofd.FileName, ".Icon"), System.Drawing.Imaging.ImageFormat.Icon);
+                }
+                 
+                else if((checkBox1.CheckState==CheckState.Unchecked) 
+                    && (checkBox2.CheckState==CheckState.Unchecked) 
+                    && (checkBox3.CheckState==CheckState.Unchecked) 
+                    && (checkBox4.CheckState==CheckState.Unchecked) 
+                    && (checkBox5.CheckState==CheckState.Unchecked) )
+                {
+                    MessageBox.Show("Change format picture", "Form1", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+                
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Form1", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //ChekBox - при указании конвертируется в Png
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //ChekBox - при указании конвертируется в Gif
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            //ChekBox - при указании конвертируется в Jpeg
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Текстовое окно, выводящее имя файла с форматом
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //Текст: "Путь к файлу"
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            checkBox1.CheckState = CheckState.Unchecked;
+            checkBox2.CheckState = CheckState.Unchecked;
+            checkBox3.CheckState = CheckState.Unchecked;
+            checkBox4.CheckState = CheckState.Unchecked;
+            checkBox5.CheckState = CheckState.Unchecked;
+            pictureBox1.Image = new Bitmap(@"D:\2 курс\4 семестр\ЭВМ\CourseWorkEVM\StartPhoto1.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            textBox1.Text = null;
+            richTextBox1.Text = null;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            //ChekBox - при указании конвертируется в Tiff
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            //ChekBox - при указании конвертируется в Icon
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
